0941a3a2094a8dac368227364dfb67c25af52386
stephen <sisong.wang@kaisquare.com>
2017-02-14 22:43:44 +0800

    Remove intermediate files, add library files that are initially ignored

--- platforms/win64/Configs.cmake
+++ platforms/win64/Configs.cmake
@@ -11,7 +11,5 @@ add_definitions(-D_WIN32_WINNT=0x0601)
 
 set(OPENCV_LIBRARYDIR ${GUIDANCE_LIB_PATH}/OpenCV/2.4.9/win64/msvc120/)
 
-find_library(Guidance_Lib DJI_guidance.lib HINTS ${GUIDANCE_SDK_LIB_PATH}/2013/x64)
+find_library(Guidance_Lib DJI_guidance.lib PATHS ${GUIDANCE_SDK_LIB_PATH}/2013/x64)
 
-set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} /GL /O2") 
-set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /GL /O2")
